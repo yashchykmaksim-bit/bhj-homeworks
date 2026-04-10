20028222cbca51be7777010efc973479c2d24f68
Maksim <yaschyk.maksim@yandex.ru>
2026-04-10 10:42:38 +0300

    second commit dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,17 +1,24 @@
-let dropDown = document.querySelector(".dropdown__value");//в переменную сохранили значение найденного элемента 
-let dropList = document.querySelector(".dropdown__list");//в переменную сохранили значение найденного элемента 
-dropDown.addEventListener("click", function folding() {// вешаем слушателя, при событии клик вызываем функцию 
-    dropList.classList.toggle("dropdown__list_active");// которая добавляет класс к нашему элементу
-}
-)
-let dropLink = document.querySelectorAll(".dropdown__link");//в переменную сохранили значение найденного элемента 
-let arr = [...dropLink];// преобразуем псевдомассив в массив 
-for (let i = 0; i < arr.length; i++) {// цикл for используем , что-бы на все элементы повесить слушателя
-    arr[i].addEventListener("click", function (event) {//здесь я не очень понимаю(вешаем слушателя при событии клик )
-        dropList.classList.remove("dropdown__list_active");// удаляем класс
-        event.preventDefault();// запрещаем переходить по ссылкам. 
-        dropDown.textContent = event.currentTarget.innerText;// меняем значение у свойств объекта 
-        console.log(event);// зачем-то выводим в консоль объект событие 
+let dropDowns = document.querySelectorAll(".dropdown");// здесь мы находим наш контейнер на странице(их может быть нескольеко)
+let array = [...dropDowns];// пробразовали в массив 
+array.forEach((item) => {// циклом перебираем наши элементы 
+    let dropDown = item.querySelector(".dropdown__value");// находим внутри контейнера (элемент JavaScript)
+    let dropList = item.querySelector(".dropdown__list");// контейнер в котором лежит список ссылок
+    let dropLink = item.querySelectorAll(".dropdown__link");// это наши ссылки в которых содеражаться названия языков
+    let arr = [...dropLink];// так это псевдоколлекция преобразуем в массив 
+    for (let i = 0; i < arr.length; i++) {// циклом вешаем слушателя на каждый элемент
+        arr[i].addEventListener("click", function (event) {
+            dropList.classList.remove("dropdown__list_active");// добавляем класс текущему элементу 
+            event.preventDefault();// отменяем стандартное действие браузера, запрещаем переход по ссылкам 
+            dropDown.textContent = event.currentTarget.textContent;// здесь мы меняем текст (не очень помню как это правильно описать)
+        }
+        )
+    }
+    dropDown.addEventListener("click", function folding() {// вешаем слушателя и при событии клик переключаем класс
+        dropList.classList.toggle("dropdown__list_active");
     }
     )
-}
+});
+
+
+
+
