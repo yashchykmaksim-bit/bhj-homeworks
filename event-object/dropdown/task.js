15448702b1478441e4d477d038362caf8fbca797
Maksim <yaschyk.maksim@yandex.ru>
2026-04-10 10:42:38 +0300

    second commit dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,17 +1,24 @@
-let dropDown = document.querySelector(".dropdown__value");//в переменную сохранили значение найденного элемента 
-let dropList = document.querySelector(".dropdown__list");//в переменную сохранили значение найденного элемента 
-dropDown.addEventListener("click", function folding() {// вешаем слушателя, при событии клик вызываем функцию 
-    dropList.classList.toggle("dropdown__list_active");// которая добавляет класс к нашему элементу
-}
-)
-let dropLink = document.querySelectorAll(".dropdown__link");//в переменную сохранили значение найденного элемента 
-let arr = [...dropLink];// преобразуем псевдомассив в массив 
-for (let i = 0; i < arr.length; i++) {// цикл for используем , что-бы на все элементы повесить слушателя
-    arr[i].addEventListener("click", function (event) {//здесь я не очень понимаю(вешаем слушателя при событии клик )
-        dropList.classList.remove("dropdown__list_active");// удаляем класс
-        event.preventDefault();// запрещаем переходить по ссылкам. 
-        dropDown.textContent = event.currentTarget.innerText;// меняем значение у свойств объекта 
-        console.log(event);// зачем-то выводим в консоль объект событие 
+let dropDowns = document.querySelectorAll(".dropdown");
+let array = [...dropDowns];
+array.forEach((item) => {
+    let dropDown = item.querySelector(".dropdown__value");
+    let dropList = item.querySelector(".dropdown__list");
+    let dropLink = item.querySelectorAll(".dropdown__link");
+    let arr = [...dropLink];
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].addEventListener("click", function (event) {
+            dropList.classList.remove("dropdown__list_active");
+            event.preventDefault();
+            dropDown.textContent = event.currentTarget.textContent;
+        }
+        )
+    }
+    dropDown.addEventListener("click", function folding() {
+        dropList.classList.toggle("dropdown__list_active");
     }
     )
-}
+});
+
+
+
+
