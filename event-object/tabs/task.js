f45b26c6d9d147bbc19d0f7dec5a63ceef4bc524
Maksim <yaschyk.maksim@yandex.ru>
2026-04-13 21:02:42 +0300

    second commit tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,13 +1,19 @@
-let tabs = document.querySelectorAll(".tab");
-let arr = [...tabs];
- let tabContents = document.querySelectorAll(".tab__content");
-        let array = [...tabContents];
-for (let i = 0; i < arr.length; i++) {
-    arr[i].addEventListener("click", function showTab() {
-        document.querySelector(".tab_active").classList.remove("tab_active");
-        arr[i].classList.add("tab_active");
-        document.querySelector(".tab__content_active").classList.remove("tab__content_active");
-        array[i].classList.add("tab__content_active");
-    }
-    )
-}
+
+let tabsContainers = document.querySelectorAll(".tabs");
+
+tabsContainers.forEach((container) => {
+
+    let tabs = container.querySelectorAll(".tab");
+    let contents = container.querySelectorAll(".tab__content");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+  
+            container.querySelector(".tab_active")?.classList.remove("tab_active");
+            container.querySelector(".tab__content_active")?.classList.remove("tab__content_active");
+
+            tab.classList.add("tab_active");
+            contents[index].classList.add("tab__content_active");
+        });
+    });
+});
